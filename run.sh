1537753d205a5979236df314e80456d5297d0ddc
akhil1999 <akhileshbhuvanendra@gmail.com>
2020-12-12 15:56:42 +0530

    initial commit

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "Akhilesh's C++ C&E Script"
+echo " "
+echo "compiling your program..."
+echo " "
+g++ -o run cpp.cpp
+echo "running your program..."
+echo " "
+echo "Program has started"
+./run
